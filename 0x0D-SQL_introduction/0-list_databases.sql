14ee401f03c220624aeea1ef164184542568182b
zaynabkrm <zaynabelkaroumi@gmail.com>
2024-09-20 16:14:47 -0700

    list databases

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,6 @@
--- lists all databases of the MySQL server
-SHOW DATABES;
+-- Connect to the MySQL server
+-- Use your credentials to log in
+-- For example:
+-- mysql -u your_username -p
+-- Run the following command to list all databases
+SHOW DATABASES;
