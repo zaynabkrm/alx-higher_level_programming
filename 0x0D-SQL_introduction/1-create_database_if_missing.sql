5736378da3031b9816990148aec3d09f63d92190
zaynabkrm <zaynabelkaroumi@gmail.com>
2024-09-20 17:33:20 -0700

    create database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,7 +1,5 @@
 -- Connect to the MySQL server
 -- Use your credentials to log in
--- For example:
--- mysql -hlocalhost -uroot -p
 
--- Creates the database hbtn_0c_0
+-- Creates the database hbtn_0c_0 in the MySQL server
 CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0';
