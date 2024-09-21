0486d7662cb12098a54816ed0703884de073ef5f
zaynabkrm <zaynabelkaroumi@gmail.com>
2024-09-21 06:25:09 -0700

    Full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Prints the full description of the table first_table from the database hbtn_0c_0
+-- CREATE TABLE IF NOT EXISTS first_table (id INT , name VARCHAR(256));
+SHOW CREATE TABLE first_table;
