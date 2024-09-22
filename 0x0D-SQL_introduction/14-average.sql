f56943298da53a6a60a7760f30785be73d59a248
zaynabkrm <zaynabelkaroumi@gmail.com>
2024-09-21 17:05:05 -0700

    Average

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,4 @@
--- computes the score average of all records in the table second_table of the database hbtn_0c_0
-SELECT AVG(score) AS average_score 
+-- Computes the score average of all records in the table second_table of the database hbtn_0c_0
+
+SELECT AVG(score) AS average
 FROM second_table;
