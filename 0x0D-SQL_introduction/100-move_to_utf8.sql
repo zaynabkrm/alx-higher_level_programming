d0418fa468cfdff003b7cfe16b1081982fbfb183
zaynabkrm <zaynabelkaroumi@gmail.com>
2024-09-25 09:55:18 -0700

    GO to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,6 +2,6 @@
 -- Convert the table to UTF8
 -- Convert the field to UTF8
 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+ALTER DATABASE 'hbtn_0c_0' CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table	CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table	MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
